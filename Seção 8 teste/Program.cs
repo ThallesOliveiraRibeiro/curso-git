ee9110c3974b75928902438af9c9e6068e223a6e
Thalles Oliveira <manobalboa@outlook.com>
2020-05-31 17:40:04 -0300

    Acrescentada uma mensagem de bom dia

--- Seção 8 teste/Program.cs
+++ Seção 8 teste/Program.cs
@@ -4,6 +4,7 @@ namespace Seção_8_teste {
     class Program {
         static void Main(string[] args) {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Bom dia");
             Console.ReadLine();
         }
     }
