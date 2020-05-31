afd776774d32f6740792b455e36488b0b453de1e
Thalles Oliveira <manobalboa@outlook.com>
2020-05-31 17:35:42 -0300

    Projeto incial mostrando a mensagem Hello World

---
+++ Seção 8 teste/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Seção_8_teste {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
