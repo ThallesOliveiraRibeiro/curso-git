01364195aab74896097b5abb01685cb7749e17bf
Thalles Oliveira <manobalboa@outlook.com>
2020-06-02 17:18:19 -0300

    Acrescentada uma pergunta de Como voce esta hoje?

--- Seção 8 teste/Program.cs
+++ Seção 8 teste/Program.cs
@@ -5,6 +5,7 @@ namespace Seção_8_teste {
         static void Main(string[] args) {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Bom dia");
+            Console.WriteLine("Como voce esta hoje?");
             Console.ReadLine();
         }
     }
