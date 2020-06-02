55692c4ff4aa2dd2ceabfe04479d75b33563417f
Thalles Oliveira <manobalboa@outlook.com>
2020-06-02 18:08:51 -0300

    Adicionado traços

--- Seção 8 teste/Program.cs
+++ Seção 8 teste/Program.cs
@@ -3,10 +3,12 @@
 namespace Seção_8_teste {
     class Program {
         static void Main(string[] args) {
+            Console.WriteLine("------------------------------------");
             Console.WriteLine("Hello World!");
             Console.WriteLine("Bom dia");
             Console.WriteLine("Como voce esta hoje?");
             Console.ReadLine();
+            Console.WriteLine("------------------------------------");
         }
     }
 }
